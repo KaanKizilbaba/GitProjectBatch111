7cf805530a992f15c2269a7e367e6ea833d35f07
KaanKizilbaba <kizilbabaa@gmail.com>
2021-03-01 18:36:13 +0100

    deneme

---
+++ src/gün1/BirinciGün.java
@@ -0,0 +1,12 @@
+package gün1;
+
+public class BirinciGün {
+
+	public static void main(String[] args) {
+		
+		System.out.println("birinci gün");
+		System.out.println("1. commit");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GitProjectBatch111 {
+}
