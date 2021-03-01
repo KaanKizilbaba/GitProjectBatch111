6d3b819e4fa57538678bd6fe7e6b77557025ab6d
KaanKizilbaba <79786667+KaanKizilbaba@users.noreply.github.com>
2021-03-01 21:35:08 +0100

    Update BirinciGün.java

--- src/gün1/BirinciGün.java
+++ src/gün1/BirinciGün.java
@@ -1,16 +1,17 @@
-package gün1;
+package gÃ¼n1;
 
-public class BirinciGün {
+public class BirinciGÃ¼n {
 
 	public static void main(String[] args) {
 		
-		System.out.println("birinci gün");
+		System.out.println("birinci gÃ¼n");
 		System.out.println("1. commit");
 
-		System.out.println("20:36 - ikinci gün");
-		System.out.println("20:50 ikinci gün");
-		System.out.println("21.01 ikinci gün");
+		System.out.println("20:36 - ikinci gun");
+		System.out.println("20:50 ikinci gun");
+		System.out.println("21.01 ikinci gun");
 		System.out.println("20:16 - local");
+		System.out.println("21:35 github.com");
 
 
 	}
